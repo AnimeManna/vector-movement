4834cb4ed0f5714487c2d1a234d53740464df2f2
AnimeManna <maxuniwersal@gmail.com>
2025-01-31 21:08:17 +0200

    Updated component to draw guard with ligthing

--- MovementVectorApp.cpp
+++ MovementVectorApp.cpp
@@ -5,18 +5,19 @@
 #include "raylib.h"
 using namespace std;
 
-
-const int indexOfX = 0;
-const int indexOfY = 1;
+struct CustomVector {
+    float x;
+    float y;
+};
 
 // Legacy code to run it only in console. But console can't handle event like mouse click, so use graph design
-void drawMatrix(float firstPoint[2], float secondPoint[2]) {
+void drawMatrix(CustomVector firstPoint, CustomVector secondPoint) {
     const int matrixSizeX = 25;
     const int matrixSizeY = 50;
 
     for (int i = 1; i <= matrixSizeX; i++) {
         for (int j = 1; j <= matrixSizeY; j++) {
-            if (firstPoint[indexOfX] == i && firstPoint[indexOfY] == j || secondPoint[indexOfX] == i && secondPoint[indexOfY] == j) {
+            if (firstPoint.x == i && firstPoint.y == j || secondPoint.x == i && secondPoint.y == j) {
                 cout << "@";
             }
             else {
@@ -28,26 +29,54 @@ void drawMatrix(float firstPoint[2], float secondPoint[2]) {
 
 }
 
-void calculateMovementForPoint(float firstPoint[2], float secondPoint[2], float speed) {
-
-    int resultVector[2] = { secondPoint[indexOfX] - firstPoint[indexOfX], secondPoint[indexOfY] - firstPoint[indexOfY] };
-
+CustomVector getNormalaziedVector(CustomVector firstPoint, CustomVector secondPoint){
+     CustomVector resultVector = { secondPoint.x - firstPoint.x, secondPoint.y - firstPoint.y };
 
 
-    double resultVectorModule = sqrt((resultVector[indexOfX] * resultVector[indexOfX]) + (resultVector[indexOfY] * resultVector[indexOfY]));
+
+    float resultVectorModule = sqrt((resultVector.x * resultVector.x) + (resultVector.y * resultVector.y));
 
     if (resultVectorModule < 0.01f) {
-        return;
+        return CustomVector{0.01f, 0.01f};
     }
 
-    double normalizedVector[2] = { resultVector[indexOfX] / resultVectorModule, resultVector[indexOfY] / resultVectorModule };
+    CustomVector normalizedVector = { resultVector.x / resultVectorModule, resultVector.y / resultVectorModule };
 
-    firstPoint[indexOfX] = firstPoint[indexOfX] + normalizedVector[indexOfX] * speed;
-    firstPoint[indexOfY] = firstPoint[indexOfY] + normalizedVector[indexOfY] * speed;
+    return normalizedVector;
 }
 
-bool isArrayHasSameCoords  (float firstPoint[2], float secondPoint[2]){
-    return firstPoint[indexOfX] == secondPoint[indexOfX] && firstPoint[indexOfY] == secondPoint[indexOfY];
+CustomVector calculateMovementForPoint(CustomVector firstPoint, CustomVector secondPoint, float speed) {
+
+    CustomVector normalizedVector = getNormalaziedVector(firstPoint, secondPoint);
+    return CustomVector{ firstPoint.x + normalizedVector.x * speed,firstPoint.y + normalizedVector.y * speed };
+}
+
+bool isVectorHasSameCoords  (CustomVector firstPoint, CustomVector secondPoint){
+    return firstPoint.x == secondPoint.x && firstPoint.y == secondPoint.y;
+}
+
+void drawCharacter(CustomVector characterPosition, CustomVector rotation, float characterSize, Color color) {
+    float wayLineLength = 10.0f + characterSize;
+    DrawLine(characterPosition.x, characterPosition.y,  characterPosition.x + rotation.x * wayLineLength, characterPosition.y + rotation.y * wayLineLength, BLUE);
+    DrawCircle(characterPosition.x, characterPosition.y, characterSize, color);
+}
+
+void drawGuard(CustomVector guardPosition, float guardSize, float currentTime, Color color) {
+    float fovDistance = 20.0f + guardSize;
+    float fovDigrese = 60.0f;
+    float fovHalfRadians = (fovDigrese / 2) * (PI / 180.0f);
+    DrawCircle(guardPosition.x, guardPosition.y, guardSize, GRAY);
+    CustomVector fovPosition = { guardPosition.x + cos(currentTime) * fovDistance, guardPosition.y + sin(currentTime) * fovDistance };
+    Vector2 firstTriangleAngle = {
+        fovPosition.x + cos(currentTime + fovHalfRadians) * fovDistance,
+        fovPosition.y + sin(currentTime + fovHalfRadians) * fovDistance
+    };
+    Vector2 secondTriangleAngle = {
+        fovPosition.x + cos(currentTime - fovHalfRadians) * fovDistance,
+        fovPosition.y + sin(currentTime - fovHalfRadians) * fovDistance
+    };
+    Vector2 formattedGuardPosition = { guardPosition.x, guardPosition.y };
+    DrawTriangle(formattedGuardPosition, firstTriangleAngle, secondTriangleAngle, YELLOW);
 }
 
 int main() {
@@ -57,9 +86,15 @@ int main() {
 
 
     float speed = 1;
-    float firstPoint[2] = { 25, 25 };
-    float secondPoint[2] = { 150, 150 };
     float pointSize = 10;
+    float guardSize = 20.0f;
+    CustomVector firstPoint = { 25, 25 };
+    CustomVector secondPoint = { 150, 150 };
+    CustomVector rotation = { 0, 0 };
+
+    CustomVector guardPosition = { 300, 300 };
+    float guardRotationSpeed = 1.0f;
+
 
     InitWindow(windowWidth, windowHeight, "Raylib Test");
 
@@ -68,20 +103,21 @@ int main() {
 
         if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
             Vector2 mousePos = GetMousePosition();
-            secondPoint[indexOfX] = mousePos.x;
-            secondPoint[indexOfY] = mousePos.y;
+            secondPoint.x = mousePos.x;
+            secondPoint.y = mousePos.y;
         }
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        if (!isArrayHasSameCoords(firstPoint, secondPoint)) {
+        if (!isVectorHasSameCoords(firstPoint, secondPoint)) {
             
             float frameTime = GetFrameTime();
-            calculateMovementForPoint(firstPoint, secondPoint, speed * frameTime * 200);
+            firstPoint = calculateMovementForPoint(firstPoint, secondPoint, speed * frameTime * 200);
+             rotation = getNormalaziedVector(firstPoint, secondPoint);
+            drawCharacter(firstPoint, rotation, pointSize, GREEN);
         }
-
-        DrawCircle(firstPoint[indexOfX], firstPoint[indexOfY], pointSize, GREEN);
-        DrawCircle(secondPoint[indexOfX], secondPoint[indexOfY], pointSize, RED);
+        DrawCircle(secondPoint.x, secondPoint.y, pointSize, RED);
+        drawGuard(guardPosition, guardSize, GetTime(), GRAY);
 
 
 
